7fb6ff6f7daee5be6423a733fe9f6780aef16585
Mattisv <mattis.vainikainen@gmail.com>
2019-09-04 03:19:08 +0200

    added enemies AI
    added Abstract class and using virtual, override.. Made AI on MossGiant with 2  waypoints.

---
+++ Assets/Assets/Scripts/Enemy/MossGiant.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossGiant : Enemy
+{
+    private Vector3 _currentTarget;
+    private Animator _anim;
+
+    private void Start() 
+    {
+       _anim = GetComponentInChildren<Animator>();
+    }
+
+
+    public override void Update()
+    {
+       if(_anim.GetCurrentAnimatorStateInfo(0).IsName("idle")) 
+       {
+           return;
+       }
+       Movement();
+    }
+
+    void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            _anim.SetTrigger("idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            _anim.SetTrigger("idle"); 
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+
+    }
+}
