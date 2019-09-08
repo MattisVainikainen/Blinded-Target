094e25df44593bfdf1d0acc78ffb3bd845816234
Mattisv <mattis.vainikainen@gmail.com>
2019-09-08 13:43:21 +0200

    Completed shop with 3 items
    Only awar so far is key to castle

---
+++ Assets/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("Hey UI manager = null, please add one");
+            }
+            return _instance;
+        }
+    }
+
+    public Text playerGemCountText;
+    public Image selectionImage;
+
+    public void OpenShop(int gemCount)
+    {
+        playerGemCountText.text = "" + gemCount + "G"; 
+    }
+
+    public void UpdateShopSelection(int yPos)
+    {
+        selectionImage.rectTransform.anchoredPosition = new Vector2(selectionImage.rectTransform.anchoredPosition.x, yPos);
+    }
+
+    void Awake() 
+    {
+        _instance = this; 
+    }
+
+}
