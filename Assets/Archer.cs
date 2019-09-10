da8f7d3edb18f346ef8cab8b12285a2122076c9e
Mattis Vainikainen <Mattis.vainikainen@gmail.com>
2019-09-10 22:59:19 +0200

    3d game start

---
+++ Assets/Archer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Enemy, IDamageable
+{
+    public int Health { get; set; }
+    public override void Init()
+    {
+        base.Init();
+        Health = base.health;
+    }
+
+    public override void MoveMent()
+    {
+        base.MoveMent();
+        float distance = Vector3.Distance(player.transform.localPosition, transform.localPosition);
+
+        Vector3 direction = player.transform.localPosition - transform.localPosition;
+        if (direction.x > 0 && anim.GetBool("inCombat") == true)
+        {
+            sprite.flipX = false;
+            // transform.localScale = new Vector3(3, 3, 3);
+        }
+        else if (direction.x < 0 && anim.GetBool("inCombat") == true)
+        {
+            sprite.flipX = true;
+            // transform.localScale = new Vector3(-3, 3, 3);
+        }
+    }
+
+    public void Damage()
+    {
+        /*   if (isDead == false)
+            {
+              Health -= 1;
+              anim.SetTrigger("hit");
+              isHit = true;
+              anim.SetBool("inCombat", true);
+
+              if (Health < 1)
+              {
+                  isDead = true;
+                  anim.SetTrigger("death");
+                  GameObject diamond = Instantiate(diamondPrefab, transform.position, Quaternion.identity) as GameObject;
+                  diamond.GetComponent<Diamond>().gems = base.gems;
+              }
+            }*/
+      }
+    }
